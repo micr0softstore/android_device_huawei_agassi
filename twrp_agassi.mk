fec8a2a634473d0dcc0c382e1e7db5913b76e011
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-12 19:40:15 +0100

    Update device and product details for TWRP Agassi

--- twrp_agassi.mk
+++ twrp_agassi.mk
@@ -9,16 +9,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from  device
-$(call inherit-product, device/agassi//device.mk)
+$(call inherit-product, device/huawei/agassi/device.mk)
 
-PRODUCT_DEVICE := 
-PRODUCT_NAME := omni_
-PRODUCT_BRAND := Agassi
-PRODUCT_MANUFACTURER := agassi
+PRODUCT_DEVICE := agassi
+PRODUCT_NAME := twrp_agassi
+PRODUCT_BRAND := Huawei
+PRODUCT_MANUFACTURER := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-agassi
 
